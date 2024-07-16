e626a623211f4f853dd05ceba68d0f9d255b11b2
Erlantz Calvo <erlantz.calvo@incotel.es>
2024-07-16 17:35:34 +0200

    Add download manager

--- index.js
+++ index.js
@@ -1,44 +1,10 @@
-import colors from 'colors'
 import { TorrentInfo } from './torrent/torrentInfo.js'
-import { Peer } from './connection/peer.js'
-import {Queue} from './managers/queue.js'
+import { DownloadManager } from './managers/download-manager.js'
 
 const torrentFile = new TorrentInfo('tests/torrent-files/debian.torrent', true)
 // const torrentFile = new TorrentInfo('tests/torrent-files/big-buck-bunny.torrent')
 // const torrentFile = new TorrentInfo('tests/torrent-files/test.torrent', true)
 
-const peersInfo = await torrentFile.makeAnnounceRequest()
-console.log(colors.green('Peers number:', peersInfo.peers?.length))
 
-let currentPeer = 0
-let piecesQueue = new Queue(torrentFile.getPiecesNumber())
-
-connectPeer(peersInfo.peers, currentPeer++)
-
-// const interval = setInterval(() => {
-// console.log('Interval')
-// }, 5000)
-
-function connectPeer (peers, index) {
-  console.log('Connecting to peer ', index)
-  const peer = getPeer(peers, index)
-  peer.connect()
-
-  peer.on('timeout', () => {
-    console.log('Peer timeout: ', index)
-    if (index === peers.length) index = -1
-    connectPeer(peers, index + 1)
-  })
-
-  peer.on('peer-error', (error) => {
-    // console.log('Peer connection error: ', index, error)
-    if (index === peers.length) index = -1
-    connectPeer(peers, index + 1)
-  })
-}
-
-function getPeer (peers, index) {
-  const { ip, port } = peers[index]
-  const peerId = peers[index]['peer id']
-  return new Peer(ip, port, peerId, torrentFile.infoHash, piecesQueue)
-}
+const downloadManager = new DownloadManager(torrentFile, 4)
+downloadManager.start()
---
+++ managers/download-manager.js
@@ -0,0 +1,93 @@
+import { Peer } from "../connection/peer.js";
+import { TorrentInfo } from "../torrent/torrentInfo.js";
+import colors from 'colors'
+
+
+export class DownloadManager {
+  /**
+   * 
+   * @param {TorrentInfo} torrent 
+   * @param {number} maxPeersNumber 
+   */
+  constructor(torrent, maxPeersNumber) {
+    this._torrent = torrent
+    this.maxPeersNumber = maxPeersNumber
+    this.peersListInfo = null
+    this._connectedPeers = {}
+    this._connectedPeersNumber = 0
+    this._lastPeerIndex = 0
+    this._refreshPeersInterval = null
+  }
+
+  async start() {
+    const manager = this
+    this.refreshPeers()
+    this._refreshPeersInterval = setInterval(() => manager.refreshPeers(), manager.peersListInfo?.interval * 1000 || 900000)
+  }
+  
+  async fetchPeersList() {
+    this.peersListInfo = await this._torrent.makeAnnounceRequest()
+  }
+
+  async refreshPeers() {
+    await this.fetchPeersList()
+    this.refreshPeerConnections()
+  } 
+
+  refreshPeerConnections() {
+    const remainingConnections = this.maxPeersNumber - this._connectedPeersNumber
+    for (let i = 0; i < remainingConnections; i++) {
+      const newPeer = this._connectPeer(this._lastPeerIndex, this.peersListInfo)
+      this._connectedPeers[this._lastPeerIndex] = newPeer
+      this._increasePeerIndex();
+      this._connectedPeersNumber++
+    }
+  }
+
+  
+  stop() {
+    clearInterval(this._refreshPeersInterval)
+    this._closeAllPeersConections()
+  }
+  
+  _increasePeerIndex() {
+    this._lastPeerIndex++;
+    if (this._lastPeerIndex >= this.peersListInfo.peers.length) this._lastPeerIndex = 0;
+  }
+
+  _connectPeer(peerIdx) {
+    console.log('Connecting to peer ', peerIdx)
+    const peer = this._getPeer(peerIdx)
+    peer.connect()
+  
+    peer.on('timeout', () => {
+      console.log(colors.yellow('Peer timeout: ', peerIdx))
+      this._finishPeerConnection(peerIdx)
+      this.refreshPeerConnections()
+    })
+  
+    peer.on('peer-error', (error) => {
+      this._finishPeerConnection(peerIdx)
+      this.refreshPeerConnections()
+    })
+  
+    return peer
+  }
+
+  _getPeer (peerIdx) {
+    const { ip, port } = this.peersListInfo.peers[peerIdx]
+    const peerId = this.peersListInfo.peers[peerIdx]['peer id']
+    return new Peer(ip, port, peerId, this._torrent )
+  }
+
+  _finishPeerConnection(peerIdx) {
+    if(this._connectedPeers[peerIdx]) {
+      delete this._connectedPeers[peerIdx]
+      this._connectedPeersNumber--
+    }
+  }
+
+  _closeAllPeersConections() {
+    Object.keys(this._connectedPeers).forEach(peer => this._finishPeerConnection(peer))
+  }
+}
