6c9db6fa6ad1f58273a609edf8c0864ea39370c9
Erlantz <erlantzcalvocarrillo@gmail.com>
2025-01-07 16:16:45 +0100

    Remove unnnecesary torrent files

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import { TorrentInfo } from './torrent/torrentInfo.js'
 import { DownloadManager } from './managers/download-manager.js'
 
-const torrentFile = new TorrentInfo('tests/torrent-files/debian.torrent', true)
-// const torrentFile = new TorrentInfo('tests/torrent-files/big-buck-bunny.torrent')
-// const torrentFile = new TorrentInfo('tests/torrent-files/test.torrent', true)
+const torrentFile = new TorrentInfo('tests/torrent-files/test.torrent', true)
 
-const downloadManager = new DownloadManager(torrentFile, 4)
+const downloadManager = new DownloadManager(torrentFile, 50)
 downloadManager.start()
